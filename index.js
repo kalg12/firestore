d04bc998fe2fa219a267f325af79012606b3f2cb
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-08-03 21:30:16 -0500

    Adding data

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ import {db} from './firebase/firebase.js'
 /* import {collection, addDoc} from 'firebase/firestore/lite'
  */
 import { collection, addDoc } from "firebase/firestore";
+
+
+/* Agregando datos a un documento */
+try {
+  const docRef = await addDoc(collection(db, "personas"), {
+    nombre: "Juan",
+    apellidos: "Pérez",
+    edad: 30
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
