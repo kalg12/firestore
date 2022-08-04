9fc00a8225843009c6eea24434fd152f2ffd2313
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-08-03 21:28:20 -0500

    Normal index without changes

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 import {db} from './firebase/firebase.js'
 /* import {collection, addDoc} from 'firebase/firestore/lite'
  */
-
 import { collection, addDoc } from "firebase/firestore";
-
-try {
-  const docRef = await addDoc(collection(db, "personas"), {
-    nombre: "Juan",
-    apellidos: "Pérez",
-    edad: 30
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-
-
